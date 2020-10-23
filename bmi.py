73f00be376b5121f853e6cac7df46335c30731d2
Salman <SNAQVI@ibs-b.hu>
2020-10-23 15:23:51 +0200

    innit

---
+++ bmi.py
@@ -0,0 +1,5 @@
+massInKG = 81.2
+heightInM = 1.78
+
+BMI = massInKG // heightInM
+print(BMI)
---
+++ swap.py
@@ -0,0 +1,8 @@
+a = 123
+b = 526
+temp = a
+a = b
+b = temp
+
+print('The value of a after swapping: {}'.format(a))
+print('The value of b after swapping: {}'.format(b))
