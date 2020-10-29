bd5cd74312a60876974b2e773e474a22d36b6ca4
Salman Raza <SNAQVI@ibs-b.hu>
2020-10-29 05:40:47 +0100

    Changes made in Variable Mutation

--- variable_mutation.py
+++ variable_mutation.py
@@ -1,4 +1,36 @@
 a = 3
-b = a + 10
-print(b)
+print(a + 10)
+
+b = 100
+print(b - 7)
+
+c = 44
+print(c * 2)
+
+d = 125
+print(d / 5)
+
+e = 8
+print(e ** 3)
+
+f1 = 123
+f2 = 345
+print(f1>f2)
+
+g1 = 350
+g2 = 200
+print((g2 * 2)>g1)
+
+h = 1357988018575474
+print(bool(h/11))
+
+i1 = 10
+i2 = 3
+print(bool(i1 > (i2 ** 2)))
+print(bool(i1 < (i2 ** 3)))
+
+j = 1521
+
+print(bool(j%3 == 0))
+print(bool(j%5 == 0))
 
