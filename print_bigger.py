304d6b8265263421d2701c714fb1073949afd562
Salman Raza <SNAQVI@ibs-b.hu>
2020-11-04 03:14:56 +0100

    Adding Print Bigger

---
+++ print_bigger.py
@@ -0,0 +1,9 @@
+# Write a program that asks for two numbers and prints the bigger one
+
+a = int(input("Enter Number: "))
+b = int(input("Enter Second Number: "))
+
+if a>b:
+    print(a)
+else:
+    print(b)
